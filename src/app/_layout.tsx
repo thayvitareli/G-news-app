96c5cd21de8afcba76984434841a7d91962b15be
thayvitareli <thainyvitareli@hotmail.com>
2026-03-09 20:54:47 -0300

    chore: config QueryClientProvider

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,18 @@
 import { Stack } from "expo-router";
 import '../../global.css'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
 
 export default function RootLayout() {
+    const [queryClient] = useState(() => new QueryClient());
+
   return (
+     <QueryClientProvider client={queryClient}>
+
     <Stack screenOptions={{ headerShown: false }} >
       <Stack.Screen name="index" />
       <Stack.Screen name="(tabs)" />
     </Stack>
+     </QueryClientProvider>
   );
 }

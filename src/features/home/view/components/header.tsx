5b6e39d47caee6c9d06b98862ddedad8fe5f2788
thayvitareli <thainyvitareli@hotmail.com>
2026-03-10 19:28:57 -0300

    feat: HomeHeader component

---
+++ src/features/home/view/components/header.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+import dayjs from 'dayjs'
+
+export const HomeHeader = () => (
+  <View className="w-full flex-row items-center justify-between px-5 py-5">
+    <View className="gap-2">
+      <Text className="text-2xl font-bold text-primary">GNews Portal</Text>
+      <Text className="text-sm text-gray-500">{dayjs().format('DD [de] MMM [de] YYYY')}</Text>
+    </View>
+  </View>
+);
